64c225689fa43827bece1a6a4681b7ebb3f7d348
Michael <fidd.michael@gmail.com>
2023-07-04 21:26:23 +0200

    setup complete

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import Notiflix from "notiflix";
+import SlimSelect from "slim-select";
+import axios from "axios";
+
+axios.defaults.headers.common["x-api-key"] = "live_C6zv0egZ9uR34lryeOyk0iT3v8royBB2i8madCVsyelmd9h8qXvgOoSYtk4Gqi6W";
+
