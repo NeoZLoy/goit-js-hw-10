9d53a515c72a372ce269662c5400331531af8645
Michael <fidd.michael@gmail.com>
2023-07-11 08:12:10 +0200

    parcel setuop

--- src/js/create-select-elemet.js
+++
@@ -1,4 +0,0 @@
-export default function createSelectElement({ id, name }) {
-    return `<option data-placeholder="true"></option>;
-    <option value="${id}">${name || 'Unknown'}</option>`;
-  }
--- src/js/get-args.js
+++
@@ -1,9 +0,0 @@
-export default function getArgs({ url, breeds }) {
-    const { name, description, temperament } = breeds[0];
-    return {
-      url,
-      name,
-      description,
-      temperament,
-    };
-}
